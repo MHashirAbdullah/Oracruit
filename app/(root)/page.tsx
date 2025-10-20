8e352fd1ed50ec39ee82689845e4cdfb6d8aa13c
MHashirAbdullah <hashirabdullah46@gmail.com>
2025-10-20 14:33:11 +0500

    install dayjs for using date, root(layout) created navbar, root(page) implemented hero banner, interview c ards with respective sectons, implemented interview-card component, create techIcons diplay components for displaying in in terview-cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
-
+import { Button } from "@/components/ui/button";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { dummyInterviews } from "@/constants";
+import InterviewCard from "@/components/InterviewCard";
 const page = () => {
   return (
-    <div className='text-5xl text-red-200'>Home Page</div>
-  )
-}
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI Powered Practice & FeedBack</h2>
+          <p className="text-lg">
+            Practice on Real Interview & Get Instant Feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
 
-export default page
+        <Image
+          src="/robot.png"
+          alt="robot"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      {/* YOUR INTERVIEW SECTION */}
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+          {/* <p>There are no interviews available.</p> */}
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default page;
