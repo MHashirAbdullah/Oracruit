781885c809a66f5681b015cd8a3ada351ebb5dbb
MHashirAbdullah <hashirabdullah46@gmail.com>
2025-10-21 00:55:13 +0500

    use env variables for client.ts while securing environment variables

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB--yj9CZgkJ-mou2vkvnYGifpxBVVBSok",
-  authDomain: "oracruit.firebaseapp.com",
-  projectId: "oracruit",
-  storageBucket: "oracruit.firebasestorage.app",
-  messagingSenderId: "667380340025",
-  appId: "1:667380340025:web:223a3d9507ed42a7db04e8",
-  measurementId: "G-1BZQTSHSR3",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
